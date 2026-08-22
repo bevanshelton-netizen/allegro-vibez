17e3b408c023495ea1f81550032aa2520629c10c
bevanshelton-netizen <bevanshelton@gmail.com>
2026-08-22 20:34:17 +0200

    Activate artist-first experience across discovery hub and signup

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import './styles/app.css'
 import './styles/landing-upgrade.css'
 import './styles/artist-magnet.css'
 import './styles/platform-experience.css'
+import './styles/auth-experience.css'
 import './homeArtistExperience.js'
 import './platformExperience.js'
+import './authExperience.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
