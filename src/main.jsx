d2a9018c9507c27baa89dec349cc8b2d3a9019bc
bevanshelton-netizen <bevanshelton@gmail.com>
2026-08-22 20:26:48 +0200

    Activate artist-first ALLEGRO VIBEZ homepage

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/app.css'
 import './styles/landing-upgrade.css'
+import './styles/artist-magnet.css'
+import './homeArtistExperience.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
